3a8957ff328c1a1ee5dc5ae9ac35c8fc5a281ca5
leemyou <limhyeon0809@gmail.com>
2024-10-20 17:14:57 +0900

    DESIGN : header UI
    - header 기본 ui

---
+++ src/containers/Header/style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const StyledHeader = styled.div`
+  height: 40px;
+  padding: 0 40px;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  align-items: center;
+  column-gap: 8px;
+`;
