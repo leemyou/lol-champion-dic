7e207d6d08b0c98d65a6555bfcd6357734ca2e7e
leemyou <limhyeon0809@gmail.com>
2024-10-21 17:37:51 +0900

    CHORE: vite로 마이그레이션

---
+++ src/main.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import CssBaseline from "@mui/material/CssBaseline";
+import reportWebVitals from "./reportWebVitals";
+import { ThemeProvider } from "@mui/material";
+import { muiTheme } from "./styles/ThemeMuiStyle";
+
+// getElementById가 null이 아님을 보장
+const rootElement = document.getElementById("root") as HTMLElement;
+
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+    <ThemeProvider theme={muiTheme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
