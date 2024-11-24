8414acb9d7ebe3418e55d7e181ff607bc377eeb6
leemyou <limhyeon0809@gmail.com>
2024-11-24 20:22:21 +0900

    Fire : 사용하지 않는 코드 제거
    - 사용하지 않는 코드 제거
    - 지역 이미지 삭제
    - 지역 데이터에서 이미지 부분 삭제

--- src/containers/index.ts
+++ src/containers/index.ts
@@ -1,6 +1,4 @@
-export * from "./ChampionCard";
 export * from "./DetailModal";
 export * from "./FilterDrawer";
 export * from "./Header";
-export * from "./Main";
 export * from "./NodeContainer";
